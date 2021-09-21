e757ec4877cd1e873113d9500d601c49415720c9
Nichita <nichita.macheev@gmail.com>
2021-09-21 20:38:53 +0300

    new repo

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+let number = 5; 
+
+
+const myArray = {
+    age: 100,
+    firstName: "Bill",
+    lastName: "Mat"
+};
+myArray.age = 50;
+// console.log(4/0);
+// console.log('string ' / 9);
+// console.log(myArray);
+// let arr = ['Nick ', 25, true]; 
+// console.log(arr.toString);
+
+// alert(arr);
+// let result = confirm("You are here");
+// console.log(result);
+// let answer = +prompt("Are you 18 ?","18");
+// myArray.age = answer;
+// console.log(myArray);
+// console.log(typeof(+answer));
+// answer = +answer;
+// console.log(typeof(answer));
+// let category = "lol";
+// console.log(`https://google.com/${category}/5`);
+
+let user = "Nichita";
+alert(`привет,${user}!`);
+console.log(5%2);
