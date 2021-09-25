9ce171d91cc4f9cf65027d6c0124f1079b0c9733
Nichita <nichita.macheev@gmail.com>
2021-09-25 13:43:44 +0300

    my js app

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 "use strict";
-let number = 5; 
+// let number = 5; 
 
 
-const myArray = {
-    age: 100,
-    firstName: "Bill",
-    lastName: "Mat"
-};
-myArray.age = 55;
+// const myArray = {
+//     age: 100,
+//     firstName: "Bill",
+//     lastName: "Mat"
+// };
+// myArray.age = 55;
 // console.log(4/0);
 // console.log('string ' / 9);
 // console.log(myArray);
@@ -26,6 +26,23 @@ myArray.age = 55;
 // let category = "lol";
 // console.log(`https://google.com/${category}/5`);
 
-let user = "Nichita";
-alert(`привет,${user}!`);
-console.log(5%2);
+// let user = "Nichita";
+// alert(`привет,${user}!`);
+// console.log(5%2);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели ', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Какой последний фильм вы смотрели',''),
+      b = prompt('На сколько оцените его ?',''),
+      c = prompt('Какой последний фильм вы смотрели',''),
+      d = prompt('На сколько оцените его ?','');
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+      console.log(personalMovieDB);
